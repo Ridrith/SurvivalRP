6ae15d74d3105c14974b8fe205b427366c192b70
Ridrith <59886289+Ridrith@users.noreply.github.com>
2025-07-07 21:08:57 -0500

    Add files via upload

--- SurvivalRP/Core/FoodSystem.lua
+++ SurvivalRP/Core/FoodSystem.lua
@@ -1,25 +1,147 @@
 -- Food and drink detection system
 
 SurvivalRP.foodDatabase = {
-    -- Bread
-    [117] = {type = "food", restore = 15, name = "Tough Jerky"},
-    [2070] = {type = "food", restore = 20, name = "Darnassian Bleu"},
-    [4541] = {type = "food", restore = 25, name = "Freshly Baked Bread"},
-    [8952] = {type = "food", restore = 30, name = "Roasted Quail"},
+    -- Basic Food (3-9 restore)
+    [117] = {type = "food", restore = 6, name = "Tough Jerky"},
+    [414] = {type = "food", restore = 4, name = "Dalaran Sharp"},
+    [422] = {type = "food", restore = 5, name = "Dwarven Mild"},
+    [1487] = {type = "food", restore = 3, name = "Homemade Cherry Pie"},
+    [2070] = {type = "food", restore = 7, name = "Darnassian Bleu"},
+    [2287] = {type = "food", restore = 5, name = "Haunch of Meat"},
+    [4536] = {type = "food", restore = 4, name = "Shiny Red Apple"},
+    [4537] = {type = "food", restore = 4, name = "Tel'Abim Banana"},
+    [4538] = {type = "food", restore = 5, name = "Snapvine Watermelon"},
+    [4539] = {type = "food", restore = 4, name = "Goldenbark Apple"},
+    [4540] = {type = "food", restore = 6, name = "Tough Hunk of Bread"},
+    [4541] = {type = "food", restore = 8, name = "Freshly Baked Bread"},
+    [4542] = {type = "food", restore = 7, name = "Moist Cornbread"},
+    [4544] = {type = "food", restore = 6, name = "Mulgore Spice Bread"},
+    [4601] = {type = "food", restore = 5, name = "Soft Banana Bread"},
+    [4602] = {type = "food", restore = 6, name = "Moon Harvest Pumpkin"},
+    [4604] = {type = "food", restore = 4, name = "Forest Mushroom Cap"},
+    [4605] = {type = "food", restore = 5, name = "Red-Speckled Mushroom"},
+    [4606] = {type = "food", restore = 6, name = "Spongy Morel"},
+    [5066] = {type = "food", restore = 3, name = "Dried Fish"},
+    [5095] = {type = "food", restore = 4, name = "Rainbow Fin Albacore"},
+    [6290] = {type = "food", restore = 5, name = "Brilliant Smallfish"},
+    [6299] = {type = "food", restore = 6, name = "Sickly Looking Fish"},
+    [6887] = {type = "food", restore = 4, name = "Spotted Yellowtail"},
+    [787] = {type = "food", restore = 3, name = "Slitherskin Mackerel"},
+    [4592] = {type = "food", restore = 5, name = "Longjaw Mud Snapper"},
+    [21552] = {type = "food", restore = 7, name = "Striped Yellowtail"},
+    [8957] = {type = "food", restore = 8, name = "Spinefin Halibut"},
     
-    -- Drinks
-    [159] = {type = "drink", restore = 15, name = "Refreshing Spring Water"},
-    [1179] = {type = "drink", restore = 20, name = "Ice Cold Milk"},
-    [1645] = {type = "drink", restore = 25, name = "Moonberry Juice"},
-    [8766] = {type = "drink", restore = 30, name = "Morning Glory Dew"},
+    -- Better Food (10-15 restore)
+    [8952] = {type = "food", restore = 12, name = "Roasted Quail"},
+    [8932] = {type = "food", restore = 11, name = "Barbecued Buzzard Wing"},
+    [3726] = {type = "food", restore = 10, name = "Big Bear Steak"},
+    [3727] = {type = "food", restore = 11, name = "Hot Lion Chops"},
+    [12209] = {type = "food", restore = 13, name = "Lean Wolf Steak"},
+    [12210] = {type = "food", restore = 14, name = "Roast Raptor"},
+    [13851] = {type = "food", restore = 12, name = "Hot Wolf Ribs"},
+    [20074] = {type = "food", restore = 15, name = "Heavy Crocolisk Stew"},
+    [17222] = {type = "food", restore = 13, name = "Spider Sausage"},
+    [18045] = {type = "food", restore = 14, name = "Tender Wolf Steak"},
+    [27635] = {type = "food", restore = 15, name = "Lynx Steak"},
+    [27636] = {type = "food", restore = 14, name = "Bat Bites"},
+    [33048] = {type = "food", restore = 13, name = "Stewed Trout"},
+    [34062] = {type = "food", restore = 12, name = "Conjured Mountain Spring Water"},
     
-    -- Alcoholic beverages
-    [2593] = {type = "drink", restore = 10, name = "Flask of Port", special = "alcohol"},
-    [2594] = {type = "drink", restore = 12, name = "Flagon of Mead", special = "alcohol"},
+    -- Basic Drinks (3-9 restore)
+    [159] = {type = "drink", restore = 5, name = "Refreshing Spring Water"},
+    [1179] = {type = "drink", restore = 6, name = "Ice Cold Milk"},
+    [1205] = {type = "drink", restore = 4, name = "Melon Juice"},
+    [1708] = {type = "drink", restore = 5, name = "Sweet Nectar"},
+    [1645] = {type = "drink", restore = 7, name = "Moonberry Juice"},
+    [4791] = {type = "drink", restore = 3, name = "Enchanted Water"},
+    [5350] = {type = "drink", restore = 6, name = "Conjured Water"},
+    [8077] = {type = "drink", restore = 7, name = "Conjured Sparkling Water"},
+    [8078] = {type = "drink", restore = 8, name = "Conjured Crystal Water"},
+    [1401] = {type = "drink", restore = 4, name = "Green Tea Leaf"},
+    [2136] = {type = "drink", restore = 5, name = "Swiftthistle Tea"},
+    [18300] = {type = "drink", restore = 6, name = "Hyjal Nectar"},
+    [28399] = {type = "drink", restore = 8, name = "Filtered Draenei Water"},
+    [33042] = {type = "drink", restore = 9, name = "Black Coffee"},
     
-    -- Conjured items
-    [5349] = {type = "food", restore = 20, name = "Conjured Muffin"},
-    [8075] = {type = "drink", restore = 20, name = "Conjured Water"}
+    -- Better Drinks (10-15 restore)
+    [8766] = {type = "drink", restore = 12, name = "Morning Glory Dew"},
+    [8079] = {type = "drink", restore = 10, name = "Conjured Mineral Water"},
+    [22829] = {type = "drink", restore = 13, name = "Super Healing Potion"},
+    [28501] = {type = "drink", restore = 14, name = "Ravencrest's Legacy"},
+    [29454] = {type = "drink", restore = 11, name = "Silvermoon City Special Reserve"},
+    [32453] = {type = "drink", restore = 15, name = "Star's Lament"},
+    [33444] = {type = "drink", restore = 12, name = "Pungent Seal Whey"},
+    [33445] = {type = "drink", restore = 13, name = "Honeymint Tea"},
+    [35954] = {type = "drink", restore = 14, name = "Sweetened Goat's Milk"},
+    
+    -- Alcoholic beverages (3-15 restore)
+    [159] = {type = "drink", restore = 3, name = "Mead Basted Caribou", special = "alcohol"},
+    [1177] = {type = "drink", restore = 4, name = "Oil of Olaf", special = "alcohol"},
+    [1179] = {type = "drink", restore = 5, name = "Ice Cold Milk", special = "alcohol"},
+    [1942] = {type = "drink", restore = 4, name = "Bottle of Moonshine", special = "alcohol"},
+    [2593] = {type = "drink", restore = 6, name = "Flask of Port", special = "alcohol"},
+    [2594] = {type = "drink", restore = 7, name = "Flagon of Mead", special = "alcohol"},
+    [2595] = {type = "drink", restore = 5, name = "Jug of Badlands Bourbon", special = "alcohol"},
+    [2596] = {type = "drink", restore = 8, name = "Skin of Dwarven Stout", special = "alcohol"},
+    [3703] = {type = "drink", restore = 6, name = "Southshore Stout", special = "alcohol"},
+    [4595] = {type = "drink", restore = 4, name = "Junglevine Wine", special = "alcohol"},
+    [8928] = {type = "drink", restore = 9, name = "Dried King Bolete", special = "alcohol"},
+    [18269] = {type = "drink", restore = 10, name = "Gordok Green Grog", special = "alcohol"},
+    [18284] = {type = "drink", restore = 8, name = "Kreeg's Stout Beatdown", special = "alcohol"},
+    [20031] = {type = "drink", restore = 7, name = "Essence Mango", special = "alcohol"},
+    [21151] = {type = "drink", restore = 11, name = "Rumsey Rum Black Label", special = "alcohol"},
+    [23246] = {type = "drink", restore = 12, name = "Fiery Festival Brew", special = "alcohol"},
+    [23585] = {type = "drink", restore = 9, name = "Stormstout", special = "alcohol"},
+    [32668] = {type = "drink", restore = 13, name = "Dos Ogris", special = "alcohol"},
+    [32722] = {type = "drink", restore = 14, name = "Ichor of Undeath", special = "alcohol"},
+    [34832] = {type = "drink", restore = 15, name = "Captain Rumsey's Lager", special = "alcohol"},
+    
+    -- Rare/Special Food (16-25 restore)
+    [5349] = {type = "food", restore = 18, name = "Conjured Muffin"},
+    [8075] = {type = "drink", restore = 20, name = "Conjured Water"},
+    [19696] = {type = "food", restore = 22, name = "Harvest Bread"},
+    [20516] = {type = "food", restore = 20, name = "Bobbing Apple"},
+    [21023] = {type = "food", restore = 19, name = "Dirge's Kickin' Chimaerok Chops"},
+    [21217] = {type = "food", restore = 25, name = "Sagefish Delight"},
+    [21552] = {type = "food", restore = 17, name = "Striped Yellowtail"},
+    [22019] = {type = "food", restore = 21, name = "Conjured Croissant"},
+    [22895] = {type = "food", restore = 23, name = "Conjured Cinnamon Roll"},
+    [28486] = {type = "food", restore = 24, name = "Moser's Magnificent Muffin"},
+    [29448] = {type = "food", restore = 16, name = "Mag'har Grainbread"},
+    [29449] = {type = "food", restore = 18, name = "Bladespire Bagel"},
+    [29450] = {type = "food", restore = 20, name = "Telaari Grapes"},
+    [29451] = {type = "food", restore = 22, name = "Clefthoof Jerky"},
+    [33872] = {type = "food", restore = 25, name = "Spicy Hot Talbuk"},
+    [35563] = {type = "food", restore = 24, name = "Charred Bear Kabobs"},
+    [35565] = {type = "food", restore = 23, name = "Juicy Bear Burger"},
+    [38427] = {type = "food", restore = 21, name = "Pickled Egg"},
+    [38428] = {type = "food", restore = 19, name = "Rock-Salted Pretzel"},
+    
+    -- Rare/Special Drinks (16-25 restore)
+    [19299] = {type = "drink", restore = 18, name = "Fizzy Faire Drink"},
+    [19300] = {type = "drink", restore = 17, name = "Bottled Winterspring Water"},
+    [23161] = {type = "drink", restore = 20, name = "Refreshing Red Apple"},
+    [28112] = {type = "drink", restore = 22, name = "Underspore Pod"},
+    [29395] = {type = "drink", restore = 19, name = "Etherlium Essence"},
+    [32455] = {type = "drink", restore = 24, name = "Star's Tears"},
+    [33093] = {type = "drink", restore = 21, name = "Mana Berries"},
+    [33096] = {type = "drink", restore = 23, name = "Fizzy Faire Drink 'Classic'"},
+    [38429] = {type = "drink", restore = 25, name = "Blackrock Spring Water"},
+    [38430] = {type = "drink", restore = 20, name = "Blackrock Mineral Water"},
+    
+    -- Holiday/Event Items
+    [21213] = {type = "food", restore = 15, name = "Gingerbread Cookie"},
+    [21215] = {type = "food", restore = 12, name = "Graccu's Mince Meat Fruitcake"},
+    [21254] = {type = "drink", restore = 10, name = "Winter Veil Egg Nog"},
+    [34068] = {type = "food", restore = 8, name = "Weighted Jack-o'-Lantern"},
+    [37604] = {type = "food", restore = 16, name = "Tooth Pick"},
+    
+    -- Potions that could be considered drinks
+    [929] = {type = "drink", restore = 12, name = "Healing Potion"},
+    [1710] = {type = "drink", restore = 15, name = "Greater Healing Potion"},
+    [3928] = {type = "drink", restore = 18, name = "Superior Healing Potion"},
+    [13446] = {type = "drink", restore = 21, name = "Major Healing Potion"},
+    [22829] = {type = "drink", restore = 24, name = "Super Healing Potion"}
 }
 
 -- This function replaces the placeholder in the main file
@@ -57,7 +179,7 @@ function SurvivalRP:HandleEating()
         end
     else
         -- Generic food consumption
-        self:HandleConsumption("food", 20)
+        self:HandleConsumption("food", 8) -- Lowered from 20
         if self.config.enableEmotes then
             local message = "eats some food and feels less hungry."
             if self.config.chatMode == "ADDON" then
@@ -90,7 +212,7 @@ function SurvivalRP:HandleDrinking()
         end
     else
         -- Generic drink consumption
-        self:HandleConsumption("drink", 25)
+        self:HandleConsumption("drink", 6) -- Lowered from 25
         if self.config.enableEmotes then
             local message = "drinks something and feels less thirsty."
             if self.config.chatMode == "ADDON" then
--- SurvivalRP/Core/Temperature.lua
+++ SurvivalRP/Core/Temperature.lua
@@ -15,49 +15,304 @@ function SurvivalRP:UpdateTemperature()
         self.playerData.temperature = self.playerData.temperature + adjustment
     end
     
-    -- Clamp temperature
-    self.playerData.temperature = math.max(0, math.min(100, self.playerData.temperature))
+    -- Clamp temperature to new range
+    self.playerData.temperature = math.max(-50, math.min(150, self.playerData.temperature))
     
     -- Apply temperature effects
     self:ApplyTemperatureEffects()
 end
 
 function SurvivalRP:GetTargetTemperature()
-    local baseTemp = 50 -- Normal temperature
+    local baseTemp = 68 -- Normal comfortable temperature
     
-    -- Zone-based temperature
+    -- Get current zone and subzone
     local zone = GetZoneText()
-    local zoneTemps = {
-        ["Winterspring"] = 10,
-        ["Icecrown"] = 5,
-        ["Tanaris"] = 85,
-        ["Uldum"] = 80,
-        ["Stranglethorn Vale"] = 75,
-        ["Dun Morogh"] = 15
+    local subzone = GetSubZoneText()
+    
+    -- Extreme Cold Zones (-50 to 19)
+    local extremeColdZones = {
+        ["Icecrown"] = -40,
+        ["Icecrown Glacier"] = -45,
+        ["Icecrown Citadel"] = -35,
+        ["Dragonblight"] = -20,
+        ["Northrend"] = -25,
+        ["Winterspring"] = -15,
+        ["Alterac Mountains"] = -10,
+        ["Coldarra"] = -50,
+        ["The Nexus"] = -30,
+        ["Storm Peaks"] = -35,
+        ["Sholazar Basin"] = 5, -- Warmer due to being protected
+        ["Borean Tundra"] = -25,
+        ["Howling Fjord"] = -15,
+        ["Grizzly Hills"] = 10,
+        ["Crystalsong Forest"] = -5,
+        ["Zul'Drak"] = -10,
+        ["The Undercity"] = 15, -- Underground but still cold
+        ["Tirisfal Glades"] = 20, -- Dead lands, perpetually cool
     }
     
-    if zoneTemps[zone] then
-        baseTemp = zoneTemps[zone]
+    -- Cold Zones (20 to 59)
+    local coldZones = {
+        ["Dun Morogh"] = 25,
+        ["Ironforge"] = 45, -- Warmer due to forges
+        ["Loch Modan"] = 35,
+        ["Wetlands"] = 40,
+        ["Arathi Highlands"] = 50,
+        ["Hillsbrad Foothills"] = 45,
+        ["Silverpine Forest"] = 35,
+        ["Darkshore"] = 40,
+        ["Ashenvale"] = 55,
+        ["Felwood"] = 30, -- Corrupted and cold
+        ["Azshara"] = 50,
+        ["Teldrassil"] = 55,
+        ["Darnassus"] = 60, -- Protected by World Tree
+        ["Moonglade"] = 58,
+        ["Desolace"] = 45, -- Barren and cool
+        ["Thousand Needles"] = 55, -- Desert but elevated
+    }
+    
+    -- Temperate Zones (60 to 78)
+    local temperateZones = {
+        ["Elwynn Forest"] = 70,
+        ["Stormwind City"] = 72,
+        ["Westfall"] = 68,
+        ["Redridge Mountains"] = 65,
+        ["Duskwood"] = 62,
+        ["Stranglethorn Vale"] = 75,
+        ["Booty Bay"] = 78,
+        ["The Barrens"] = 72,
+        ["Orgrimmar"] = 75,
+        ["Mulgore"] = 70,
+        ["Thunder Bluff"] = 68,
+        ["Dustwallow Marsh"] = 74,
+        ["Feralas"] = 72,
+        ["Un'Goro Crater"] = 76, -- Warm but not extreme due to vegetation
+        ["Silithus"] = 65, -- Desert but cooler due to silithid influence
+        ["Gadgetzan"] = 75,
+        ["Theramore"] = 74,
+        ["Goldshire"] = 71,
+        ["Lakeshire"] = 66,
+        ["Darkshire"] = 60,
+        ["Sentinel Hill"] = 69,
+    }
+    
+    -- Warm Zones (79 to 98)
+    local warmZones = {
+        ["Tanaris"] = 85,
+        ["Uldum"] = 88,
+        ["Badlands"] = 82,
+        ["Searing Gorge"] = 90,
+        ["Burning Steppes"] = 85,
+        ["Eastern Plaguelands"] = 80, -- Unnatural heat from corruption
+        ["Western Plaguelands"] = 79,
+        ["Hellfire Peninsula"] = 95,
+        ["Zangarmarsh"] = 82, -- Humid and warm
+        ["Nagrand"] = 80,
+        ["Blade's Edge Mountains"] = 85,
+        ["Netherstorm"] = 88, -- Arcane energy creates heat
+        ["Shadowmoon Valley"] = 92,
+        ["Terokkar Forest"] = 83,
+        ["Shattrath City"] = 81,
+    }
+    
+    -- Extreme Heat Zones (99 to 150)
+    local extremeHeatZones = {
+        ["Blackrock Mountain"] = 130,
+        ["Blackrock Depths"] = 145,
+        ["Blackwing Lair"] = 135,
+        ["Molten Core"] = 150,
+        ["Blackrock Spire"] = 125,
+        ["The Slag Pit"] = 140,
+        ["Firelands"] = 150,
+        ["Mount Hyjal"] = 110, -- Fire-scarred but recovering
+        ["Felwood"] = 105, -- In corrupted areas
+        ["The Tainted Scar"] = 120,
+        ["Magtheridon's Lair"] = 145,
+        ["Gruul's Lair"] = 115,
+    }
+    
+    -- Check for temperature matches
+    if extremeColdZones[zone] then
+        baseTemp = extremeColdZones[zone]
+    elseif extremeColdZones[subzone] then
+        baseTemp = extremeColdZones[subzone]
+    elseif coldZones[zone] then
+        baseTemp = coldZones[zone]
+    elseif coldZones[subzone] then
+        baseTemp = coldZones[subzone]
+    elseif temperateZones[zone] then
+        baseTemp = temperateZones[zone]
+    elseif temperateZones[subzone] then
+        baseTemp = temperateZones[subzone]
+    elseif warmZones[zone] then
+        baseTemp = warmZones[zone]
+    elseif warmZones[subzone] then
+        baseTemp = warmZones[subzone]
+    elseif extremeHeatZones[zone] then
+        baseTemp = extremeHeatZones[zone]
+    elseif extremeHeatZones[subzone] then
+        baseTemp = extremeHeatZones[subzone]
     end
     
-    -- Indoor vs outdoor (simplified check)
+    -- Special subzone adjustments
+    local subzoneAdjustments = {
+        -- Inns and taverns (warmer)
+        ["The Prancing Pony"] = 15,
+        ["Lion's Pride Inn"] = 15,
+        ["The Slaughtered Lamb"] = 12,
+        ["Thunderbrew Distillery"] = 20, -- Extra warm from brewing
+        ["The Blue Recluse"] = 10,
+        ["Pig and Whistle Tavern"] = 15,
+        ["Deeprun Tram"] = 10,
+        
+        -- Caves and underground areas
+        ["The Stockade"] = 8,
+        ["Gnomeregan"] = 5,
+        ["Deadmines"] = 10,
+        ["Shadowfang Keep"] = -5,
+        ["The Wailing Caverns"] = 12,
+        ["Ragefire Chasm"] = 25,
+        ["Razorfen Kraul"] = 8,
+        ["Razorfen Downs"] = 5,
+        ["Uldaman"] = 15,
+        ["Zul'Farrak"] = 20,
+        ["Maraudon"] = 12,
+        ["Temple of Atal'Hakkar"] = 18,
+        ["Stratholme"] = -8, -- Undead influence
+        ["Scholomance"] = -10, -- Necromantic energies
+        ["Dire Maul"] = 8,
+        
+        -- Specific hot areas
+        ["The Cauldron"] = 25, -- In Searing Gorge
+        ["Lava Pool"] = 35,
+        ["The Furnace"] = 40,
+        ["Thorium Point"] = 20,
+        ["Kargath"] = 18,
+        ["Flame Crest"] = 30,
+        
+        -- Specific cold areas
+        ["Frostwhisper Gorge"] = -15,
+        ["Lake Mereldar"] = -8,
+        ["Chillwind Camp"] = -10,
+        ["Everlook"] = -5,
+        ["Frostsaber Rock"] = -12,
+        ["Ice Thistle Hills"] = -18,
+        
+        -- Water areas (cooling effect)
+        ["Mystral Lake"] = -5,
+        ["Crystal Lake"] = -8,
+        ["Lordamere Lake"] = -3,
+        ["Darrowmere Lake"] = -5,
+        ["The Great Sea"] = -10,
+        ["Menethil Harbor"] = -3,
+        
+        -- Magical areas
+        ["Dalaran"] = 5, -- Magically climate controlled
+        ["Violet Citadel"] = 8,
+        ["Karazhan"] = -5, -- Arcane energies create cold
+        ["Tempest Keep"] = 10,
+        ["Silvermoon City"] = 12, -- Magical warmth
+        ["Exodar"] = 8, -- Climate controlled
+    }
+    
+    -- Apply subzone adjustments
+    if subzoneAdjustments[subzone] then
+        baseTemp = baseTemp + subzoneAdjustments[subzone]
+    end
+    
+    -- Indoor vs outdoor adjustments
     if IsIndoors() then
-        baseTemp = baseTemp + 10 -- Indoors is warmer
+        if string.find(subzone, "Inn") or string.find(subzone, "Tavern") then
+            baseTemp = baseTemp + 20 -- Inns are much warmer
+        elseif string.find(subzone, "Forge") or string.find(subzone, "Smithy") then
+            baseTemp = baseTemp + 25 -- Forges are very hot
+        elseif string.find(subzone, "Mine") or string.find(subzone, "Cave") then
+            baseTemp = baseTemp + 5 -- Underground is slightly warmer
+        else
+            baseTemp = baseTemp + 10 -- General indoor warmth
+        end
     end
     
-    return math.max(0, math.min(100, baseTemp))
+    -- Time of day adjustments (if available)
+    local hour = tonumber(date("%H"))
+    if hour then
+        if hour >= 22 or hour <= 6 then
+            baseTemp = baseTemp - 8 -- Nighttime is cooler
+        elseif hour >= 12 and hour <= 16 then
+            baseTemp = baseTemp + 5 -- Midday is warmer
+        end
+    end
+    
+    -- Weather adjustments (if you have weather detection)
+    -- This would require additional weather detection code
+    
+    -- Clamp to our new temperature range
+    return math.max(-50, math.min(150, baseTemp))
 end
 
 function SurvivalRP:ApplyTemperatureEffects()
-    -- Cold effects
-    if self.playerData.temperature < 20 then
-        -- Increase fatigue decay in cold
-        -- We don't modify the config directly, just apply temporary effects
+    local temp = self.playerData.temperature
+    
+    -- Extreme cold effects (below 20)
+    if temp < 20 then
+        if temp < -20 then
+            -- Severe hypothermia effects
+            -- Could add health drain, movement speed reduction, etc.
+        elseif temp < 0 then
+            -- Moderate hypothermia effects
+            -- Increase fatigue decay, slight movement penalty
+        else
+            -- Mild cold effects
+            -- Slight fatigue increase
+        end
     end
     
-    -- Hot effects
-    if self.playerData.temperature > 80 then
-        -- Increase thirst decay in heat
-        -- We don't modify the config directly, just apply temporary effects
+    -- Extreme heat effects (above 99)
+    if temp > 99 then
+        if temp > 130 then
+            -- Severe hyperthermia effects
+            -- Could add health drain, faster thirst decay
+        elseif temp > 115 then
+            -- Moderate hyperthermia effects
+            -- Significant thirst increase
+        else
+            -- Mild heat effects
+            -- Increased thirst decay
+        end
+    end
+    
+    -- Comfortable range (60-78) - no negative effects
+    if temp >= 60 and temp <= 78 then
+        -- Optimal temperature range
+        -- Could provide small bonuses or just no penalties
+    end
+end
+
+-- Helper function to get temperature status text
+function SurvivalRP:GetTemperatureStatus()
+    local temp = self.playerData.temperature
+    
+    if temp < -30 then
+        return "Freezing", {r = 0.2, g = 0.6, b = 1.0}
+    elseif temp < 0 then
+        return "Very Cold", {r = 0.4, g = 0.8, b = 1.0}
+    elseif temp < 20 then
+        return "Cold", {r = 0.6, g = 0.9, b = 1.0}
+    elseif temp < 40 then
+        return "Cool", {r = 0.8, g = 0.95, b = 1.0}
+    elseif temp < 60 then
+        return "Mild", {r = 1.0, g = 1.0, b = 0.9}
+    elseif temp <= 78 then
+        return "Comfortable", {r = 0.9, g = 1.0, b = 0.9}
+    elseif temp < 90 then
+        return "Warm", {r = 1.0, g = 1.0, b = 0.7}
+    elseif temp < 99 then
+        return "Hot", {r = 1.0, g = 0.9, b = 0.5}
+    elseif temp < 115 then
+        return "Very Hot", {r = 1.0, g = 0.7, b = 0.3}
+    elseif temp < 130 then
+        return "Scorching", {r = 1.0, g = 0.5, b = 0.2}
+    else
+        return "Infernal", {r = 1.0, g = 0.2, b = 0.1}
     end
 end
